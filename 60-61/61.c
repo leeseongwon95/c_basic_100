89036d34aca3ae268080d69f03c8411c14f6b308
Lee Seong Won <syanggu@naver.com>
2020-11-26 17:43:12 +0900

    기초 삼한연산

---
+++ 60-61/61.c
@@ -0,0 +1,22 @@
+// 두 정수 입력받아 큰 수 출력하기
+
+#include <stdio.h>
+
+int main(void) {
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+  if (a < b) {
+    if (a < c) {
+      printf("%d", a);
+    } else {
+      printf("%d", c);
+    }
+  } else {
+    if (b < c) {
+      printf("%d", b);
+    } else {
+      printf("%d", c);
+    }
+  }
+  return 0;
+}
